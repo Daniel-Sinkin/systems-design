5d5c5448d29a4cad12519b087fa369a7495b179b
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-04-02 18:51:05 +0200

    Implemented some new stuff

--- src/lib/trading.hpp
+++ src/lib/trading.hpp
@@ -3,12 +3,7 @@
 // lib/trading.hpp
 
 #include "common.hpp"
-#include <limits>
 
 namespace ds_sysdes {
-constexpr u64 k_invalid_id_64{std::numeric_limits<u64>::max()};
-constexpr u32 k_invalid_id_32{std::numeric_limits<u32>::max()};
-
 constexpr usize k_cacheline_size{64zu};
-
 } // namespace ds_sysdes
